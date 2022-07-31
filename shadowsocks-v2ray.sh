9f1c5ca36886bc2851122820fb01233b293b95d2
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2022-07-31 09:22:52 +0800

    Update shadowsocks-v2ray.sh

--- shadowsocks-v2ray.sh
+++ shadowsocks-v2ray.sh
@@ -34,7 +34,7 @@ set_domain(){
     echo "\033[1;34mPlease enter your domain:\033[0m"
     echo "If you don't have one, you can register one for free at:"
     echo "https://my.freenom.com/clientarea.php"
-    read domain "(Type your cloudfare domain):" $domain
+    read -p "(Type your cloudfare domain):" domain
     str=`echo $domain | grep '^\([a-zA-Z0-9_\-]\{1,\}\.\)\{1,\}[a-zA-Z]\{2,5\}'`
     while [ ! -n "${str}" ]
     do
