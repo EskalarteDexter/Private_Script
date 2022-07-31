a4ae2a0b7e2ba8067f611a60779b2285aff7c698
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2022-07-31 09:52:33 +0800

    Update v2rayplugin-shadowsocks.sh

--- v2rayplugin-shadowsocks.sh
+++ v2rayplugin-shadowsocks.sh
@@ -25,8 +25,8 @@ get_latest_ver(){
 set_password(){
     clear
     echo "\033[1;34mPlease enter password for shadowsocks-libev:\033[0m"
-    read -p "(Default password: liyanglan):" shadowsockspwd
-    [ -z "${shadowsockspwd}" ] && shadowsockspwd="liyanglan"
+    read -p "(Default password: dextereskalarte):" shadowsockspwd
+    [ -z "${shadowsockspwd}" ] && shadowsockspwd="dextereskalarte"
     echo "\033[1;35mpassword = ${shadowsockspwd}\033[0m"
 }
 
