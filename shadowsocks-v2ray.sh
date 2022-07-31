7974d162dbffc7dc569c3f8030e7371e028a6f79
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2022-07-31 08:40:17 +0800

    Update shadowsocks-v2ray.sh

--- shadowsocks-v2ray.sh
+++ shadowsocks-v2ray.sh
@@ -87,8 +87,8 @@ install_mbedtls(){
         fi
         tar xf mbedtls-$MBEDTLS_VER-gpl.tgz
         cd mbedtls-$MBEDTLS_VER
-        make SHARED=1 CFLAGS=-fPIC
-        make DESTDIR=/usr install
+        ./configure --prefix=/usr && make
+        make install
         cd ..
         ldconfig
         if [ ! -f /usr/lib/libmbedtls.a ];then
